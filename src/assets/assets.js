35b5cfe7ec37291a4e8afa8a953dcdfb5383e9ad
Eric Chun <eric.chun88800888@gmail.com>
2024-12-18 09:19:38 +0900

    logo image fix

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -56,7 +56,6 @@ import p_img52 from "./p_img52.png";
 
 import logo from "./logo.png";
 import logo2 from "./logo2.png";
-
 import hero_img from "./hero_img.png";
 import cart_icon from "./cart_icon.png";
 import bin_icon from "./bin_icon.png";
